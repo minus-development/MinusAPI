2e01cb4c792ddc3fb2aae99d56386ae896dc76a6
Colin Edwards <ddrboxman@gmail.com>
2011-02-12 23:43:37 -0800

    Runs through all most API calls to show how they work

--- python/create_gallery.py
+++ python/create_gallery.py
@@ -1,4 +1,7 @@
-import sys, subprocess, urllib2, os
+import sys, subprocess, urllib, urllib2, os
+from time import strftime
+import json
+import unicodedata
 
 def upload_file(filename, editor_id, key):
 	basename = os.path.basename(filename)
@@ -11,7 +14,7 @@ def upload_file(filename, editor_id, key):
 		print 'Uploaded ' + basename + ' (' + dd['filesize'] + ')'
 	else:
 		print 'Failed ' + basename + ' : ' + error
-	
+
 def parseDict(dictStr):
 	dictStr = dictStr.translate(None,'\"{}')
 	items = [s for s in dictStr.split(', ') if s]
@@ -19,22 +22,48 @@ def parseDict(dictStr):
 	for item in items:
 		key,value = item.split(': ')
 		dd[key] = value
-	
+
 	return dd	
 
 def create_gallery():
 	f = urllib2.urlopen('http://min.us/api/CreateGallery')
 	result = f.read()
 	dd = parseDict(result)
-	
-	return (dd['editor_id'], dd['key'], dd['reader_id'])
+
+	return (dd['editor_id'], "", dd['reader_id'])
+
+def saveGallery(name, editor_id, items):
+	params = {"name":name,"id": editor_id,"key":"OK","items":items}
+	encoded = urllib.urlencode(params)
+	try:
+		f = urllib2.urlopen('http://min.us/api/SaveGallery ', encoded)
+	except urllib2.HTTPError, e:
+		print "\n", e.code
+		print "SaveGallery Failed:\n", "params: ", params, "\nencoded params: ", encoded, "\n"
+
+def generateImageList(reader_id):
+
+	formattedList = []
+
+	f = urllib2.urlopen('http://min.us/api/GetItems/m' + reader_id)
+	jsonData = json.loads( f.read())
+	imagesList = jsonData[u'ITEMS_GALLERY']
+	for image in imagesList:
+		image = unicodedata.normalize('NFKD', image).encode('ascii','ignore')
+		image = image[17:]
+		image = image.split(".")[0]
+		formattedList.append(image)
+	return formattedList
+		
 
 def main():
 	editor_id,key,reader_id = create_gallery()
 	args = sys.argv[1:]
 	for arg in args:
 		upload_file(arg,editor_id,key)
+	imageList = generateImageList(reader_id)
+	saveGallery("Testing rename - " + strftime("%Y-%m-%d %H:%M"), editor_id, imageList)
 	print 'Editor URL: http://min.us/m' + editor_id 
 	print 'Viewer URL: http://min.us/m' + reader_id
-	
+
 main()
