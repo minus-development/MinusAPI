27fd2a1c83f5d140dde23a8e77d6ce9d2e6dcc4c
Ruel Pagayon <ruel@ruel.me>
2011-04-22 07:56:13 -0700

    Fixed whitespaces, python convention issues, docstrings, etc.

--- python/create_gallery.py
+++ python/create_gallery.py
@@ -1,26 +1,30 @@
+#/usr/bin/env python
+
 import sys, subprocess, urllib, urllib2, os
 from time import strftime
 import json
 import unicodedata
 
-########## REPLACE FOLLOWING WITH VALID EDITOR AND VIEWER URLS ####################
+''' 
+	REPLACE FOLLOWING WITH VALID EDITOR AND VIEWER URLS 
+'''
 MINUS_URL = 'http://min.us/'
 # MINUS_URL = 'http://192.168.0.190:8001/'
 READER_ID = "veTYOJ"
 EDITOR_ID = "cXLjZ5CjJr5J"
-###################################################################################	
+
 
 def upload_file(filename, editor_id, key):
 	basename = os.path.basename(filename)
-	url=MINUS_URL + 'api/UploadItem' + '?editor_id=' + editor_id + '&key=' + key + '&filename=' + basename
-	file_arg = 'file=@'+filename
+	url ='%sapi/UploadItem?editor_id=%s&key=%s&filename=%s' % (MINUS_URL, editor_id, key, basename)
+	file_arg = 'file=@%s' % filename
 	p = subprocess.Popen(['curl', '-s', '-S', '-F', file_arg, url], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output,error = p.communicate()
 	if p.returncode == 0:
 		dd = parseDict(output)
-		print 'Uploaded ' + basename + ' (' + dd['filesize'] + ')'
+		print 'Uploaded %s (%s)' % (basename, dd['filesize'])
 	else:
-		print 'Failed ' + basename + ' : ' + error
+		print 'Failed %s : %s' % (basename, error)
 
 def parseDict(dictStr):
 	dictStr = dictStr.translate(None,'\"{}')
@@ -33,7 +37,7 @@ def parseDict(dictStr):
 	return dd	
 
 def create_gallery():
-	f = urllib2.urlopen(MINUS_URL + 'api/CreateGallery')
+	f = urllib2.urlopen('%sapi/CreateGallery' % MINUS_URL)
 	result = f.read()
 	dd = parseDict(result)
 
@@ -41,10 +45,10 @@ def create_gallery():
 
 def saveGallery(name, editor_id, items):
 	name = "test"
-	params = {"name":name,"id": editor_id,"key":"OK","items":json.dumps(items)}
+	params = { "name" : name, "id" : editor_id, "key" : "OK", "items" : json.dumps(items) }
 	params = urllib.urlencode(params)
 	try:
-		f = urllib2.urlopen(MINUS_URL + 'api/SaveGallery', params)
+		f = urllib2.urlopen('%sapi/SaveGallery' % MINUS_URL, params)
 	except urllib2.HTTPError, e:
 		print "\n", e.code
 		print "SaveGallery Failed:\n", "params: ", params
@@ -52,7 +56,7 @@ def saveGallery(name, editor_id, items):
 def generateImageList(reader_id):
 
 	formattedList = []
-	f = urllib2.urlopen(MINUS_URL + 'api/GetItems/m' + reader_id)
+	f = urllib2.urlopen('%sapi/GetItems/m%s' % (MINUS_URL, reader_id))
 	jsonData = json.loads( f.read())
 	imagesList = jsonData[u'ITEMS_GALLERY']
 	for image in imagesList:
@@ -69,8 +73,9 @@ def main():
 	#for arg in args:
 	#	upload_file(arg,editor_id,key)
 	imageList = generateImageList(READER_ID)	
-	saveGallery("Testing rename - " + strftime("%Y-%m-%d %H:%M"), EDITOR_ID, imageList)
-	print 'Editor URL: http://min.us/m' + EDITOR_ID 
-	print 'Viewer URL: http://min.us/m' + READER_ID
+	saveGallery("Testing rename - %s" % strftime("%Y-%m-%d %H:%M"), EDITOR_ID, imageList)
+	print 'Editor URL: http://min.us/m%s' % EDITOR_ID 
+	print 'Viewer URL: http://min.us/m%s' % READER_ID
 
-main()
+if __name__ == '__main__':
+	main()
